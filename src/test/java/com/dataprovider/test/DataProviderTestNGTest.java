8ce83573a0cff7512545a14f8ac59b582d3e6740
Ankit Jain <ankitj@xpanxion.co.in>
2017-06-20 14:05:57 +0530

    Second

--- src/test/java/com/dataprovider/test/DataProviderTestNGTest.java
+++ src/test/java/com/dataprovider/test/DataProviderTestNGTest.java
@@ -1,46 +1,26 @@
 package com.dataprovider.test;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProviderTestNGTest {
+private PrimeNo prime;
 
-	public static String driverPath = "C:/chromedriver/";
-	public static WebDriver driver;
 
-	
-@BeforeTest
-	public void server() {
-		System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-	}
-@AfterMethod
-public void clearCache(){
-	String webURL=driver.getCurrentUrl();
-	System.out.println("Current URl is" + webURL);
+@BeforeMethod
+public void initialize(){
+	prime=new PrimeNo();
 }
-	
-	@Test
-	public void openURL() throws InterruptedException{
-		driver.get("https://www.google.com");
-		WebElement sEarchTextBox= driver.findElement(By.id("lst-ib"));
-		sEarchTextBox.sendKeys("wiki" + "\n");
-		Thread.sleep(3000);
-		Assert.assertTrue(sEarchTextBox.isDisplayed());
-		
-	}
-	
-	@AfterTest
-	public void cLosebrowser(){
-		driver.close();
-	}
+@DataProvider (name="test1")
+public static Object [] [] verifyPrimeNo(){
+	return new Object [][]{{2,true},{6,false},{7,true},{10,false},{17,true}};
+}
+@Test (dataProvider= "test1")
+public void primeNO(int i, Boolean result){
+System.out.println("GivenNo is  " + i + " result is " + result );	
+//System.out.println(((PrimeNo) pn).testPrimeNo(i));
+Assert.assertEquals(result, new PrimeNo().testPrimeNo(i),"Number not matching"+result+" "+prime.testPrimeNo(i));
+}
 }
